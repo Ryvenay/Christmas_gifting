f36042ec7a346ebd398bf2c6cc6fd8ad77e7760a
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-12-21 18:06:12 +0100

    Changed github link for the repo

--- src/protected/footer.php
+++ src/protected/footer.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col col-md-4">
-            <a href="https://github.com/Ryvenay" target="_blank"> <img class="GHIcon" src="<?=PUBLIC_DIR.'GitHub-Mark-Light-64px.png'; ?>"></a>
+            <a href="https://github.com/Ryvenay/Christmas_gifting" target="_blank"> <img class="GHIcon" src="<?=PUBLIC_DIR.'GitHub-Mark-Light-64px.png'; ?>"></a>
         </div>
         <div class="col col-md-4">
             © 2021
